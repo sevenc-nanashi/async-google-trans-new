37d8fe07cf7b6f055cf46f8887d7a9bedaca8b53
sevenc-nanashi <sevenc_nanashi@yahoo.co.jp>
2020-12-10 16:32:45 +0900

    This lib is async now!

--- google_trans_new.py
+++ google_trans_new.py
@@ -1,11 +1,16 @@
 # coding:utf-8
 # author LuShan
 # version : 1.1.9
-import json, requests, random, re
+import json
+import aiohttp
+import random
+import re
 from urllib.parse import quote
 import urllib3
 import logging
-from .constant import LANGUAGES, DEFAULT_SERVICE_URLS
+import aiohttp.web_exceptions
+from constant import LANGUAGES, DEFAULT_SERVICE_URLS
+import asyncio
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
@@ -96,6 +101,7 @@ class google_translator:
         url_base = "https://translate.google.{}".format(self.url_suffix)
         self.url = url_base + "/_/TranslateWebserverUi/data/batchexecute"
         self.timeout = timeout
+        self.__session = aiohttp.ClientSession()
 
     def _package_rpc(self, text, lang_src='auto', lang_tgt='auto'):
         GOOGLE_TTS_RPC = ["MkEWBc"]
@@ -108,144 +114,169 @@ class google_translator:
         freq = freq_initial
         return freq
 
-    def translate(self, text, lang_tgt='auto', lang_src='auto', pronounce=False):
+    @property
+    def _session(self):
+        if self.__session.closed:
+            self.__session = aiohttp.ClientSession()
+        return self.__session
+
+    async def translate(self, text, lang_tgt='auto', lang_src='auto', pronounce=False):
         try:
-            lang = LANGUAGES[lang_src]
-        except:
-            lang_src = 'auto'
-        try:
-            lang = LANGUAGES[lang_tgt]
-        except:
-            lang_src = 'auto'
-        text = str(text)
-        if len(text) >= 5000:
-            return "Warning: Can only detect less than 5000 characters"
-        if len(text) == 0:
-            return ""
-        headers = {
-            "Referer": "http://translate.google.{}/".format(self.url_suffix),
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; WOW64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/47.0.2526.106 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
-        }
-        freq = self._package_rpc(text, lang_src, lang_tgt)
-        response = requests.Request(method='POST',
-                                    url=self.url,
-                                    data=freq,
-                                    headers=headers,
-                                    )
-        try:
-            if self.proxies == None or type(self.proxies) != dict:
-                self.proxies = {}
-            with requests.Session() as s:
-                s.proxies = self.proxies
-                r = s.send(request=response.prepare(),
-                           verify=False,
-                           timeout=self.timeout)
-            for line in r.iter_lines(chunk_size=1024):
-                decoded_line = line.decode('utf-8')
-                if "MkEWBc" in decoded_line:
-                    try:
-                        response = (decoded_line + ']')
-                        response = json.loads(response)
-                        response = list(response)
-                        response = json.loads(response[0][2])
-                        response_ = list(response)
-                        response = response_[1][0]
-                        if len(response) == 1:
-                            if len(response[0]) > 5:
-                                sentences = response[0][5]
-                            else: ## only url
-                                sentences = response[0][0]
+            try:
+                lang = LANGUAGES[lang_src]
+            except:
+                lang_src = 'auto'
+            try:
+                lang = LANGUAGES[lang_tgt]
+            except:
+                lang_src = 'auto'
+            text = str(text)
+            if len(text) >= 5000:
+                return "Warning: Can only detect less than 5000 characters"
+            if len(text) == 0:
+                return ""
+            headers = {
+                "Referer": "http://translate.google.{}/".format(self.url_suffix),
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/47.0.2526.106 Safari/537.36",
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+            }
+            freq = self._package_rpc(text, lang_src, lang_tgt)
+
+            try:
+                if self.proxies == None or type(self.proxies) != dict:
+                    self.proxies = {}
+                timeout = aiohttp.ClientTimeout(total=self.timeout)
+                s = self._session.post(url=self.url,
+                                       data=freq,
+                                       headers=headers,
+                                       verify_ssl=False,
+                                       timeout=timeout
+                                       )
+                s.proxy = self.proxies.get("https")
+
+                r = await s
+                for line in (await r.text()).splitlines():
+                    decoded_line = line
+                    if "MkEWBc" in decoded_line:
+                        try:
+                            response = (decoded_line + ']')
+                            response = json.loads(response)
+                            response = list(response)
+                            response = json.loads(response[0][2])
+                            response_ = list(response)
+                            response = response_[1][0]
+                            if len(response) == 1:
+                                if len(response[0]) > 5:
+                                    sentences = response[0][5]
+                                else:  # only url
+                                    sentences = response[0][0]
+                                    if pronounce == False:
+                                        return sentences
+                                    elif pronounce == True:
+                                        return [sentences, None, None]
+                                translate_text = ""
+                                for sentence in sentences:
+                                    sentence = sentence[0]
+                                    translate_text += sentence.strip() + ' '
+                                translate_text = translate_text
+                                if pronounce == False:
+                                    return translate_text
+                                elif pronounce == True:
+                                    pronounce_src = (response_[0][0])
+                                    pronounce_tgt = (response_[1][0][0][1])
+                                    return [translate_text, pronounce_src, pronounce_tgt]
+                            elif len(response) == 2:
+                                sentences = []
+                                for i in response:
+                                    sentences.append(i[0])
                                 if pronounce == False:
                                     return sentences
                                 elif pronounce == True:
-                                    return [sentences,None,None]
-                            translate_text = ""
-                            for sentence in sentences:
-                                sentence = sentence[0]
-                                translate_text += sentence.strip() + ' '
-                            translate_text = translate_text
-                            if pronounce == False:
-                                return translate_text
-                            elif pronounce == True:
-                                pronounce_src = (response_[0][0])
-                                pronounce_tgt = (response_[1][0][0][1])
-                                return [translate_text, pronounce_src, pronounce_tgt]
-                        elif len(response) == 2:
-                            sentences = []
-                            for i in response:
-                                sentences.append(i[0])
-                            if pronounce == False:
-                                return sentences
-                            elif pronounce == True:
-                                pronounce_src = (response_[0][0])
-                                pronounce_tgt = (response_[1][0][0][1])
-                                return [sentences, pronounce_src, pronounce_tgt]
-                    except Exception as e:
-                        raise e
-            r.raise_for_status()
-        except requests.exceptions.ConnectTimeout as e:
+                                    pronounce_src = (response_[0][0])
+                                    pronounce_tgt = (response_[1][0][0][1])
+                                    return [sentences, pronounce_src, pronounce_tgt]
+                        except Exception as e:
+                            raise e
+                r.raise_for_status()
+                await s.close()
+            except TimeoutError as e:
+                raise e
+            except aiohttp.web_exceptions.HTTPError as e:
+                # Request successful, bad response
+                raise google_new_transError(tts=self, response=r)
+            except aiohttp.ClientConnectorError as e:
+                # Request failed
+                raise google_new_transError(tts=self)
+        except Exception as e:
+            await s.close()
             raise e
-        except requests.exceptions.HTTPError as e:
-            # Request successful, bad response
-            raise google_new_transError(tts=self, response=r)
-        except requests.exceptions.RequestException as e:
-            # Request failed
-            raise google_new_transError(tts=self)
 
-    def detect(self, text):
-        text = str(text)
-        if len(text) >= 5000:
-            return log.debug("Warning: Can only detect less than 5000 characters")
-        if len(text) == 0:
-            return ""
-        headers = {
-            "Referer": "http://translate.google.{}/".format(self.url_suffix),
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; WOW64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/47.0.2526.106 Safari/537.36",
-            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
-        }
-        freq = self._package_rpc(text)
-        response = requests.Request(method='POST',
-                                    url=self.url,
-                                    data=freq,
-                                    headers=headers)
+    async def detect(self, text):
         try:
-            if self.proxies == None or type(self.proxies) != dict:
-                self.proxies = {}
-            with requests.Session() as s:
-                s.proxies = self.proxies
-                r = s.send(request=response.prepare(),
-                           verify=False,
-                           timeout=self.timeout)
+            text = str(text)
+            if len(text) >= 5000:
+                return log.debug("Warning: Can only detect less than 5000 characters")
+            if len(text) == 0:
+                return ""
+            headers = {
+                "Referer": "http://translate.google.{}/".format(self.url_suffix),
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/47.0.2526.106 Safari/537.36",
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+            }
+            freq = self._package_rpc(text)
 
-            for line in r.iter_lines(chunk_size=1024):
-                decoded_line = line.decode('utf-8')
-                if "MkEWBc" in decoded_line:
-                    # regex_str = r"\[\[\"wrb.fr\",\"MkEWBc\",\"\[\[(.*).*?,\[\[\["
-                    try:
-                        # data_got = re.search(regex_str,decoded_line).group(1)
-                        response = (decoded_line + ']')
-                        response = json.loads(response)
-                        response = list(response)
-                        response = json.loads(response[0][2])
-                        response = list(response)
-                        detect_lang = response[0][2]
-                    except Exception:
-                        raise Exception
-                    # data_got = data_got.split('\\\"]')[0]
-                    return [detect_lang, LANGUAGES[detect_lang.lower()]]
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            # Request successful, bad response
-            log.debug(str(e))
-            raise google_new_transError(tts=self, response=r)
-        except requests.exceptions.RequestException as e:
-            # Request failed
-            log.debug(str(e))
-            raise google_new_transError(tts=self)
+            try:
+                if self.proxies == None or type(self.proxies) != dict:
+                    self.proxies = {}
+                timeout = aiohttp.ClientTimeout(total=self.timeout)
+                s = self._session.post(url=self.url,
+                                       data=freq,
+                                       headers=headers,
+                                       verify_ssl=False,
+                                       timeout=timeout
+                                       )
+                s.proxy = self.proxies.get("https")
+
+                r = await s
+                for line in (await r.text()).splitlines():
+                    decoded_line = line.decode('utf-8')
+                    if "MkEWBc" in decoded_line:
+                        # regex_str = r"\[\[\"wrb.fr\",\"MkEWBc\",\"\[\[(.*).*?,\[\[\["
+                        try:
+                            # data_got = re.search(regex_str,decoded_line).group(1)
+                            response = (decoded_line + ']')
+                            response = json.loads(response)
+                            response = list(response)
+                            response = json.loads(response[0][2])
+                            response = list(response)
+                            detect_lang = response[0][2]
+                        except Exception:
+                            raise Exception
+                        # data_got = data_got.split('\\\"]')[0]
+                        return [detect_lang, LANGUAGES[detect_lang.lower()]]
+                r.raise_for_status()
+                await s.close()
+            except TimeoutError as e:
+                raise e
+            except aiohttp.web_exceptions.HTTPError as e:
+                # Request successful, bad response
+                raise google_new_transError(tts=self, response=r)
+            except aiohttp.ClientConnectorError as e:
+                # Request failed
+                raise google_new_transError(tts=self)
+        except Exception as e:
+            await s.close()
+            raise e
+
+    async def close(self):
+        await self.__session.close()
+
+    def __del__(self):
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.__session.close())
